576f0f8368c459bedfece9c6a9ef3fef6be6850c
a9595 <andriy9595@mail.ru>
2013-10-25 14:14:48 +0200

    final

---
+++ game/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.trackBarScores = new System.Windows.Forms.TrackBar();
+            this.progressBarTime = new System.Windows.Forms.ProgressBar();
+            this.labelPercents = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.TimeSetter = new System.Windows.Forms.NumericUpDown();
+            this.HelpButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeSetter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Khaki;
+            this.StartButton.Location = new System.Drawing.Point(12, 243);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(98, 42);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Lat\'s play bro";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.GameStartClick);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(12, 12);
+            this.listBox.Name = "listBox";
+            this.listBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox.Size = new System.Drawing.Size(98, 225);
+            this.listBox.TabIndex = 1;
+            // 
+            // trackBarScores
+            // 
+            this.trackBarScores.Enabled = false;
+            this.trackBarScores.LargeChange = 1;
+            this.trackBarScores.Location = new System.Drawing.Point(125, 273);
+            this.trackBarScores.Maximum = 16;
+            this.trackBarScores.Name = "trackBarScores";
+            this.trackBarScores.Size = new System.Drawing.Size(252, 45);
+            this.trackBarScores.TabIndex = 2;
+            this.trackBarScores.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // progressBarTime
+            // 
+            this.progressBarTime.Location = new System.Drawing.Point(125, 337);
+            this.progressBarTime.Name = "progressBarTime";
+            this.progressBarTime.Size = new System.Drawing.Size(252, 23);
+            this.progressBarTime.Step = 1;
+            this.progressBarTime.TabIndex = 3;
+            // 
+            // labelPercents
+            // 
+            this.labelPercents.AutoSize = true;
+            this.labelPercents.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelPercents.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPercents.Location = new System.Drawing.Point(125, 372);
+            this.labelPercents.Name = "labelPercents";
+            this.labelPercents.Size = new System.Drawing.Size(69, 27);
+            this.labelPercents.TabIndex = 5;
+            this.labelPercents.Text = "100%";
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSeconds.Location = new System.Drawing.Point(215, 372);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(78, 27);
+            this.labelSeconds.TabIndex = 5;
+            this.labelSeconds.Text = "60 sec";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // TimeSetter
+            // 
+            this.TimeSetter.Location = new System.Drawing.Point(12, 372);
+            this.TimeSetter.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.TimeSetter.Name = "TimeSetter";
+            this.TimeSetter.Size = new System.Drawing.Size(69, 20);
+            this.TimeSetter.TabIndex = 6;
+            this.TimeSetter.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // HelpButton
+            // 
+            this.HelpButton.BackColor = System.Drawing.SystemColors.Info;
+            this.HelpButton.Location = new System.Drawing.Point(296, 372);
+            this.HelpButton.Name = "HelpButton";
+            this.HelpButton.Size = new System.Drawing.Size(81, 27);
+            this.HelpButton.TabIndex = 7;
+            this.HelpButton.Text = "Ціль гри";
+            this.HelpButton.UseVisualStyleBackColor = false;
+            this.HelpButton.Click += new System.EventHandler(this.HelpButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::game.Properties.Resources.back2;
+            this.ClientSize = new System.Drawing.Size(389, 408);
+            this.Controls.Add(this.HelpButton);
+            this.Controls.Add(this.TimeSetter);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.labelPercents);
+            this.Controls.Add(this.progressBarTime);
+            this.Controls.Add(this.trackBarScores);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "the Game";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeSetter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TrackBar trackBarScores;
+        private System.Windows.Forms.ProgressBar progressBarTime;
+        private System.Windows.Forms.Label labelPercents;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NumericUpDown TimeSetter;
+        private System.Windows.Forms.Button HelpButton;
+
+    }
+}
+
---
+++ game/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        Random _r = new Random(Environment.TickCount);
+        List<int> _generatedNumbers = new List<int>();
+        Button[] buttons = new Button[16];
+        ToolTip _myTip = new ToolTip();
+        RadioButton[] Level = new RadioButton[3];
+        int _minGenerated, _maxGenerated;
+        float CorrectSelectedDigitsCount;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateControl();
+            //Randomizer();
+        }
+
+        public new void CreateControl()
+        {
+            #region створення кнопок(круглих причьом) і навішуєм подсказкі на контроли
+
+            for (int i = 0, x = 120, y = 15; i < buttons.Length; i++, x += 65)
+            {
+                if (i % 4 == 0 && i >= 4) //і>=4 - шоб не здвигався самий перший ряд вниз
+                {
+                    y += 55;
+                    x = 120;
+                }
+                buttons[i] = new Button
+                                 {
+                                     BackColor = Color.DarkSeaGreen,
+                                     Font = new Font("Microsoft Sans Serif", 14),
+                                     Size = new Size(60, 50),
+                                     Location = new Point(x, y),
+                                     Enabled = false,
+                                     Text = "?",
+                                     Name = Convert.ToString(i) //називаєм кожну кнопку її індексом, шоб легше було доступатись
+                                 };
+                buttons[i].Click += DigitsClick;
+                //робимо регіон кнопки - круглим
+                var buttonPath = new GraphicsPath();
+                buttonPath.AddEllipse(0, 0, buttons[i].Width, buttons[i].Height);
+                Region buttonRegion = new Region(buttonPath);
+                buttons[i].Region = buttonRegion;
+                //убераєм рамку, а то не гарне коло виходить (з якойсь рамкой)
+                buttons[i].FlatStyle = FlatStyle.Flat;
+                buttons[i].FlatAppearance.BorderSize = 0;
+
+                _myTip.SetToolTip(buttons[i], "цифра"); //ставим подсказку
+                Controls.Add(buttons[i]);
+            }
+            labelPercents.Text = "0%";
+            labelSeconds.Text = "0";
+            //вішаєм подсказкі
+            _myTip.SetToolTip(StartButton, "Розпочати гру");
+            _myTip.SetToolTip(trackBarScores, "Кількість очків");
+            _myTip.SetToolTip(progressBarTime, "Залишилось часу");
+            _myTip.SetToolTip(listBox, "Правильні числа");
+            _myTip.SetToolTip(labelPercents, "Відсоток вгаданих чисел");
+            _myTip.SetToolTip(labelSeconds, "Залишилось часу");
+            _myTip.SetToolTip(TimeSetter, "Час гри (в секундах)");
+            #endregion
+
+            #region рівні складності(радіобатони)
+
+            for (int i = 0; i < 3; i++)
+                Level[i] = new RadioButton();
+            Level[0].Text = "Легко";
+            Level[0].Checked = true;
+            Level[0].Location = new Point(11, 290);
+            Level[0].ForeColor = Color.Green;
+            _myTip.SetToolTip(Level[0], "Числа від 1 до 16");
+            Level[1].Text = "Нормально";
+            Level[1].Location = new Point(11, 315);
+            Level[1].ForeColor = Color.DarkSalmon;
+            _myTip.SetToolTip(Level[1], "Числа від 1 до 30");
+            Level[2].Text = "Party Hard";
+            Level[2].Location = new Point(11, 340);
+            Level[2].ForeColor = Color.Red;
+            _myTip.SetToolTip(Level[2], "Числа від -50 до 50");
+            for (int i = 0; i < 3; i++)
+                Controls.Add(Level[i]);
+
+            #endregion
+        }
+        /// <summary>
+        /// Заповнення кнопок випадковими унікальними числами
+        /// </summary>
+        public void Randomizer()
+        {
+            _generatedNumbers.Clear();
+            do
+            {
+                int random = 0;
+                if (Level[0].Checked)//easy level selected
+                    random = _r.Next(1, 17);
+                if (Level[1].Checked) //normal
+                    random = _r.Next(1, 31);
+                if (Level[2].Checked) //partyHard
+                    random = _r.Next(-50, 51);
+                if (!_generatedNumbers.Contains(random))
+                    _generatedNumbers.Add(random);
+            } while (_generatedNumbers.Count < 16);
+            for (int i = 0; i < 16; i++)
+            {
+                buttons[i].Text = _generatedNumbers[i].ToString(CultureInfo.InvariantCulture);
+            }
+            _minGenerated = _generatedNumbers.Min();
+            _maxGenerated = _generatedNumbers.Max();
+
+        }
+        /// <summary>
+        /// Клікі по цифрам
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DigitsClick(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int digit = Convert.ToInt32(clickedButton.Text);
+            if (listBox.Items.Count != 0 && digit > Int32.Parse(listBox.Items[(listBox.Items.Count - 1)].ToString()))
+            {
+                CorrectSelectedDigitsCount++;
+                labelPercents.Text = (CorrectSelectedDigitsCount / 16 * 100).ToString(CultureInfo.InvariantCulture) + "%";
+                trackBarScores.Value = (int)CorrectSelectedDigitsCount;
+                listBox.Items.Add(digit);
+                buttons[Int32.Parse(clickedButton.Name)].Enabled = false;
+                buttons[Int32.Parse(clickedButton.Name)].BackColor = Color.Gray;
+                if (digit == _maxGenerated)
+                {
+                    //MessageBox.Show("Game over");
+                    GameOver();
+                }
+            }
+
+            if (listBox.Items.Count == 0) //при виборі самого першого числа
+            {
+                CorrectSelectedDigitsCount++;
+                labelPercents.Text = (CorrectSelectedDigitsCount / 16 * 100).ToString(CultureInfo.InvariantCulture) + "%";
+                trackBarScores.Value = (int)CorrectSelectedDigitsCount;
+                listBox.Items.Add(digit);
+                buttons[Int32.Parse(clickedButton.Name)].Enabled = false;
+                buttons[Int32.Parse(clickedButton.Name)].BackColor = Color.Gray;
+                if (digit == _maxGenerated)
+                {
+                    //MessageBox.Show("Game over");
+                    GameOver();
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Натиснення кнопки старту
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameStartClick(object sender, EventArgs e)
+        {
+            if (StartButton.Text.StartsWith("Lat"))
+            {
+                listBox.Items.Clear();
+                Randomizer();
+                for (int i = 0; i < 16; i++)
+                {
+                    buttons[i].Enabled = true;
+                    buttons[i].BackColor = Color.DarkSeaGreen;
+                }
+                for (int i = 0; i < 3; i++)
+                    Level[i].Enabled = false;
+                StartButton.Text = "Здатися. Завершити гру";
+                HelpButton.Enabled = false;
+                TimeSetter.Enabled = false;
+                labelPercents.Text = "0%";
+                labelSeconds.Text = "0";
+                CorrectSelectedDigitsCount = 0;
+
+                progressBarTime.Value = 0;
+                progressBarTime.Minimum = 0;
+                progressBarTime.Maximum = (int)TimeSetter.Value * 10;
+                progressBarTime.Step = 1;
+                timer.Start();
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+
+        /// <summary>
+        /// шо робиться при завершенні гри
+        /// </summary>
+        void GameOver()
+        {
+            timer.Stop();
+            for (int i = 0; i < 16; i++)
+            {
+                buttons[i].Enabled = false;
+                buttons[i].BackColor = Color.Gray;
+            }
+            TimeSetter.Enabled = true;
+            StartButton.Enabled = true;
+            HelpButton.Enabled = true;
+
+            string finalText = "Ви підібрали " + labelPercents.Text + " чисел \n за " +
+                               (progressBarTime.Value / 10).ToString(CultureInfo.InvariantCulture) + "секунд";
+            string rezultGeneral = "";
+            if ((CorrectSelectedDigitsCount / 16 * 100) < 30)
+                rezultGeneral = "\nВам потрібно ще трохі пограти і Ваш результат обов'язково покращиться =)";
+            else if ((CorrectSelectedDigitsCount / 16 * 100) >= 30 && (CorrectSelectedDigitsCount / 16 * 100) < 60)
+                rezultGeneral = "\nХмм.. доволі непоганий результат. Ви схильні до швидкого розвитку уваги";
+            else if ((CorrectSelectedDigitsCount / 16 * 100) >= 60 && (CorrectSelectedDigitsCount / 16 * 100) < 85)
+                rezultGeneral = "\nДуже добре. Реакція мов у професійного боксера =)";
+            else if ((CorrectSelectedDigitsCount / 16 * 100) >= 85 && (CorrectSelectedDigitsCount / 16 * 100) <= 99)
+                rezultGeneral = "\nПрекрасно. Ви майже встановили світовий рекорд. Ще трошки захоплюючої гри і результат не заставить Вас чекати";
+            else if ((CorrectSelectedDigitsCount / 16 * 100) == 100)
+                rezultGeneral = "\nОоогооо. Це неймовірно. ВИ встановили світовий рекорд!!! У Вас прекрасний потенціал до розвитку. Я в шооці прям";
+
+            MessageBox.Show(finalText + rezultGeneral, "Гру завершенно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            for (int i = 0; i < 3; i++)
+                Level[i].Enabled = true;
+            progressBarTime.Value = 0;
+            trackBarScores.Value = 0;
+            StartButton.Text = "Lat's play bro";
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (progressBarTime.Value == (int)TimeSetter.Value * 10)
+            {
+                timer.Stop();
+                GameOver();
+            }
+
+
+            progressBarTime.PerformStep();
+            labelSeconds.Text = (TimeSetter.Value - progressBarTime.Value / 10).ToString() + " sec";
+        }
+
+        private void HelpButtonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Цільи гри:натискати на цифри в порядку зростання за максимально велику швидкість.
+
+Правила: натиснути кнопку Let's play і грати
+
+Для чого грати: Британські вчені довели, що ця гра сприяє розвитку уваги та швидкості реакції.", "Правила гри", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+
+}
